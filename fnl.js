ee16833afe332e4e0251721124c8e066ec315b79
James Vickers <james@metabench.com>
2018-10-06 02:14:12 +0100

    0.0.5 - Handler improvement, [all] consistency Comments removed Prom flexibility improvement (can accept existing Promise)

--- fnl.js
+++ fnl.js
@@ -1,9 +1,5 @@
-
-
-
 // Could have one overriding fnl function?
 //  Not yet.
-
 
 // fnl()
 // fns-es6 ?
@@ -52,9 +48,7 @@
 // Observable and observer in one...
 //  observer.observe(other_observable, (my_next, my_complete, my_data))???
 
-
 // spread operators could be useful.
-
 
 // so, we want a function sequencer
 //  could change every function that gets called into an observable if it is not already
@@ -63,42 +57,15 @@ const lang = require('lang-mini');
 const Evented_Class = lang.Evented_Class;
 const get_a_sig = lang.get_a_sig;
 
-// we give it a singer inner function as its param
-
-
-// Writing the inner observable function...
-//  cll the inner function with functions found here.
-
-
-// observable((next, complete, error) => {
-// do stuff, raise events through functions here.
-//})
-
-// Not so sure we want it like this...
-
-// Filter observable
-//  Could make a new observable, or apply the filter to the same one.
-
-// filter, .filtered.
-
-// could apply a list of filters.
-
-
 const observable = (fn_inner, always_plural) => {
-
     // So that observable(orig_observable) = orig_observable
 
     if (fn_inner.__type_name === 'observable') {
         return fn_inner;
     }
 
-
     // call the inner function, it assigns event triggers / raises
-
-
     // and get three functions coming in
-
-
     // on the next tick?
     let res = new Evented_Class();
     // Not sure about this.
@@ -106,24 +73,17 @@ const observable = (fn_inner, always_plural) => {
     //setImmediate(() => {
 
     [stop, pause, resume] = fn_inner(data => {
-
         // And could apply a filter here.
         //  Could apply a number of filters.
         let passes = true;
         if (this.filters) {
-
             for (let filter of this.filters) {
                 passes = filter(data);
                 if (!passes) break;
             }
-
         }
-
         if (passes) res.raise('next', data);
     }, last_data => {
-
-
-
         //setImmediate(() => {
         //console.log('last_data', last_data);
         if (typeof last_data !== 'undefined') {
@@ -131,10 +91,7 @@ const observable = (fn_inner, always_plural) => {
         } else {
             res.raise('complete');
         }
-
         //});
-
-
     }, error => {
         res.raise('error', error);
     });
@@ -156,7 +113,6 @@ const observable = (fn_inner, always_plural) => {
         }
     }
 
-
     res.next = handler => {
         res.on('next', handler);
         return res;
@@ -166,17 +122,11 @@ const observable = (fn_inner, always_plural) => {
     res.complete = handler => {
         // then handler has not been set up yet?
         // 
-
         //setImmediate(() => {
-
         //console.log('res complete');
-
         res.completed = true;
-
         res.on('complete', handler);
         //});
-
-
         return res;
     }
     res.done = res.end = res.complete;
@@ -184,7 +134,6 @@ const observable = (fn_inner, always_plural) => {
         res.on('error', handler);
         return res;
     }
-
 
     res.filter = (fn_filter) => {
         this.filters = this.filters || [];
@@ -207,7 +156,6 @@ const observable = (fn_inner, always_plural) => {
         let obs_false = observable((next, complete, error) => {
             return [res.stop, res.pause, res.resume];
         });
-
         // without these functions here?
 
         res.next(data => {
@@ -219,7 +167,6 @@ const observable = (fn_inner, always_plural) => {
                 obs_false.raise('next', data);
             }
         });
-
         res.complete(() => {
             obs_true.raise('complete');
             obs_false.raise('complete');
@@ -232,13 +179,9 @@ const observable = (fn_inner, always_plural) => {
     }
 
     res.then = (handler) => {
-
         // the whole thing in set immediate?
-
-        console.log('then');
+        //console.log('then');
         // what if it's already resolved?
-
-
 
         let res_all = [];
         // but if next has not hapenned?
@@ -248,25 +191,28 @@ const observable = (fn_inner, always_plural) => {
             res_all.push(data);
             had_next = true;
         });
-
         // but only one entry
+        //console.log('res.completed', res.completed);
 
-        console.log('res.completed', res.completed);
+        //console.log('res_all', res_all);
+        //console.log('res.completed', res.completed);
+
+        //console.log('always_plural', always_plural);
+
 
         if (res.completed) {
             if (had_next && res_all.length > 0) {
                 if (res_all.length > 1 || always_plural) {
                     handler(res_all);
                 } else {
-                    handler(res_all[0]);
+                    //handler(res_all[0]);
+                    handler(res_all);
                 }
-
-                
             } else {
-                handler(last);
+                //handler(last);
+                handler(res_all);
             }
         } else {
-
             res.complete(last => {
                 //console.log('complete last', last);
                 //console.log('had_next', had_next);
@@ -275,24 +221,19 @@ const observable = (fn_inner, always_plural) => {
                     if (res_all.length > 1 || always_plural) {
                         handler(res_all);
                     } else {
-                        handler(res_all[0]);
+                        //handler(res_all[0]);
+                        handler(res_all);
                     }
-
-                    
                 } else {
                     handler(last);
                 }
             })
         }
-
         // and error handling
     }
 
     res.__type_name = 'observable';
     //})
-
-
-
 
     // don't want this 'target' value added to the object.
     //  useful in DOM processing but not elsewhere.
@@ -316,19 +257,11 @@ const observable = (fn_inner, always_plural) => {
 
     // could avoid the [] requirement when it can't stop, pause, resume
 
-
-
-
-
-
     // a then function?
     //  converts to promise, calls the promise's .then function
-
-
     // res stop
     //  stop sending the data on
     //  if send the message that the observation has stopped, which means that the data source coudl be stopped.
-
     return res;
 }
 
@@ -337,7 +270,8 @@ const observable = (fn_inner, always_plural) => {
 
 const seq = (q_obs) => {
     return observable((next, complete, error) => {
-        let c = 0, obs_q_item;
+        let c = 0,
+            obs_q_item;
         let process = () => {
             if (c < q_obs.length) {
                 let q_item = q_obs[c];
@@ -357,7 +291,6 @@ const seq = (q_obs) => {
             }
         }
         process();
-
         let stop = () => {
             obs_q_item.stop();
             complete();
@@ -369,22 +302,18 @@ const seq = (q_obs) => {
             obs_q_item.resume();
             //complete();
         }
-
         return [stop, pause, resume];
     });
 }
 
 const obs_to_cb = (obs, callback) => {
     let arr_all = [];
-
     //console.log('obs_to_cb callback', callback);
     //console.trace();
-
     obs.on('next', data => arr_all.push(data));
     obs.on('error', err => callback(err));
     obs.on('complete', last => {
         //console.log('arr_all.length ', arr_all.length);
-
         /*
         if (arr_all.length === 0) {
             callback(null, last);
@@ -392,13 +321,10 @@ const obs_to_cb = (obs, callback) => {
             callback(null, arr_all);
         }
         */
-
         if (typeof last !== 'undefined') {
             // don't return the array of all, they are only interim results
             // or have it as the 3rd callback param!
-
             callback(null, last, arr_all);
-
         } else {
             //console.log('callback', callback);
             callback(null, arr_all);
@@ -412,19 +338,13 @@ const obs_to_cb = (obs, callback) => {
 // takes an observable that gives arrays (pages), breaks them up
 
 const unpage = (obs) => {
-
     return observable((next, complete, error) => {
-
         obs.on('next', arr_data => {
             //console.log('arr_data', arr_data);
-
             // Specialised processing for Command_Response_Message?
-
             // An unpage function would be nice there.
             //  or each_record
-
             // Unpage would make sense because it can be used here, it's generic.
-
             if ('unpaged' in arr_data) {
                 let unpaged = arr_data.unpaged;
                 for (let c = 0, l = unpaged.length; c < l; c++) {
@@ -446,8 +366,6 @@ const unpage = (obs) => {
     });
 }
 
-
-
 const obs_or_cb = (obs, callback, always_plural) => {
     //console.log('obs_or_cb callback', callback);
     if (callback) {
@@ -455,36 +373,25 @@ const obs_or_cb = (obs, callback, always_plural) => {
         //console.log('has obs', obs);
         obs_to_cb(obs, callback);
     } else {
-
-
-
         return observable(obs, always_plural);
     }
 }
 
 const sig_obs_or_cb = (a, inner) => {
-
     // then if the last is a callback (function)
     let a2;
     //let using_cb = false;
     let callback;
-
-
     // 
-
     if (typeof a[a.length - 1] === 'function') {
         // its a callback
-
         //console.log('its a cb');
-
         a2 = Array.prototype.slice.call(a, 0, -1);
         callback = a[a.length - 1];
         //using_cb = true;
     } else if (typeof a[a.length - 1] === 'undefined') {
         // its a callback
-
         //console.log('its a cb');
-
         a2 = Array.prototype.slice.call(a, 0, -1);
         //using_cb = true;
     } else {
@@ -493,34 +400,23 @@ const sig_obs_or_cb = (a, inner) => {
     // if the last one is undefined
     //  (as in a missing callback)
 
-
     let sig = get_a_sig(a2);
 
     let obs = observable((next, complete, error) => {
-
         // no, can't return that...
         //  need to return the observable???
         //  
-
         return inner(a2, sig, next, complete, error, a2.length);
     });
 
     //console.log('obs', obs);
     //throw 'stop';
-
     return obs_or_cb(obs, callback);
-
-
-
     //let [stop, pause, resume] = inner();
-
     // need to call the inner function as though an observable gets made.
     //  maybe it's not possible / justifyable to put that handling here.
-
     //return obs_or_cb()
-
     // then call the inner function?
-
 }
 
 
@@ -554,26 +450,37 @@ const prom_or_cb = (prom, opt_cb) => {
     }
 
     if (opt_cb) {
-
-
         _prom.then((res) => {
             opt_cb(null, res);
         }, err => {
             opt_cb(err);
-        })
+        });
     } else {
         return _prom;
-
     }
 }
 
+const prom = (prom) => {
+    if (prom instanceof Promise) {
+        return prom;
+    } else {
+        // assuming its a function.
+        return new Promise(prom);
+    }
+}
 
+/*
+    Advanced Function
+
+    // Will have a function call that assists with typing / signatures.
+    //  Regex and wildcard checking would be nice.
+    //   Regex too slow
+    //   Check if the signatures match a few regular expressions and parse the 
+
+*/
 
 // Data_Source
 //  a closure where 3 functions next, complete, error can be called.
-
-
-
 
 if (require.main === module) {
 
@@ -582,18 +489,13 @@ if (require.main === module) {
     //  Get back its stop, pause, resume functions as the result in an array
 
     /*
-
     // Keeping this longer form -~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-
 
     let data_source_interval_mult = (next, complete, error) => {
         ...
         return [stop, pause, resume];
-
-
     }
-
     // -~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~
-
 
     // Other code with a data_source functionality?
 
@@ -603,9 +505,6 @@ if (require.main === module) {
     let obs = observable(data_source_interval_mult);
 
     */
-
-
-
 
     //let ds_
 
@@ -620,7 +519,6 @@ if (require.main === module) {
 
     // This looks like it could tidy up some server code.
 
-
     let obs = observable((next, complete, error) => {
         let c = 2;
         let paused = false;
@@ -631,11 +529,12 @@ if (require.main === module) {
         }
 
         let ivl = setInterval(() => {
-
             if (!paused) {
                 let v = c * 2;
                 //console.log('v', v);
-                next({ 'v': v });
+                next({
+                    'v': v
+                });
                 c++;
                 if (c > 8) {
                     stop();
@@ -649,8 +548,6 @@ if (require.main === module) {
         }, () => {
             paused = false;
         }];
-
-
     });
 
     /* .filter(data => {
@@ -671,20 +568,10 @@ if (require.main === module) {
     //test_obs();
 
     let test_then = () => {
-
-        /*
-
-        obs.then(res => {
-            console.log('res', res);
-        })
-        */
-
         (async () => {
             let res = await obs;
             console.log('awaited res', res);
         })();
-
-
     }
     test_then();
 
@@ -709,22 +596,14 @@ if (require.main === module) {
         });
 
         obs.next(data => {
-
-            console.log('data', data);
-
+            //console.log('data', data);
             if (data === 8) {
-
-
                 obs.delay(5000);
-
-
 
                 // works nicely here.
                 // want listeners for the pause and resume events.
 
-
                 // That would be convenient syntax
-
 
                 obs.pause();
                 console.log('paused');
@@ -733,87 +612,11 @@ if (require.main === module) {
                     obs.resume();
                 }, 2000);
 
-
-
             }
         }).end(() => {
             console.log('end');
         })
-
-
     }
-
-    //test_filter();
-
-
-
-
-
-    /*
-
-    obs.next(data => {
-
-        console.log('data', data);
-
-        if (data === 8) {
-
-
-            obs.delay(5000);
-
-
-
-            // works nicely here.
-            // want listeners for the pause and resume events.
-
-
-            // That would be convenient syntax
-
-            / *
-
-            obs.pause();
-            console.log('paused');
-            setTimeout(() => {
-                console.log('wait over');
-                obs.resume();
-            }, 2000);
-
-            * /
-
-
-        }
-    }).end(() => {
-        console.log('end');
-    })
-
-    */
-
-
-
-    /*
-
-    obs.on('next', data => {
-        console.log('data', data);
-
-        // stop ir if data is 8 or more...
-
-        if (data === 8) {
-            obs.pause();
-            console.log('paused');
-            setTimeout(() => {
-                console.log('wait over');
-                obs.resume();
-            }, 2000);
-        }
-
-        //obs.stop();
-
-    })
-    */
-    /*
-     obs.on('complete', () => {
-         console.log('complete');
-     });
-     */
 
 } else {
 
@@ -826,7 +629,7 @@ module.exports = {
     'sig_obs_or_cb': sig_obs_or_cb,
     'cb_to_prom_or_cb': cb_to_prom_or_cb,
     'prom_or_cb': prom_or_cb,
+    'prom': prom,
     'obs_or_cb': obs_or_cb,
     'unpage': unpage
 }
-
